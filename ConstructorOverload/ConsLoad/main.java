4fce18544a40163e2169a6672989e387f41b257a
iCX <mralaarabi@gmail.com>
2025-09-28 23:24:53 +0100

    Constructor Overload

---
+++ ConstructorOverload/ConsLoad/main.java
@@ -0,0 +1,16 @@
+package ConsLoad;
+
+public class main {
+
+	public static void main(String[] args) {
+		Product tapeMeasure = new Product("Tape measure");
+		Product plaster = new Product("Plaster", "home improvement section");
+		Product tyre = new Product("Tyre", 5);
+
+		System.out.println(tapeMeasure);
+		System.out.println(plaster);
+		System.out.println(tyre);
+
+	}
+
+}
